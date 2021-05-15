e9d83c2b9d923151d1fcb3c882889c9f10beca60
AikChun <iam@simaikchun.com>
2021-05-15 08:12:10 +0800

    Implemented code to deploy a contract in the test file.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,14 +3,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let fetchedAccounts;
+let inbox;
 beforeEach(async () => {
   fetchedAccounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there'],
+    })
+    .send({ from: fetchedAccounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(fetchedAccounts);
+    console.log(inbox);
   });
 });
