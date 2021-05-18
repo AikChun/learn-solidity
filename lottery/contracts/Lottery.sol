43e51d467646a1b87826cd9568af4b87f19f0d5d
AikChun <iam@simaikchun.com>
2021-05-18 11:58:25 +0800

    Added a new project to deploy and test Lottery contract.

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint256 index = random() % players.length;
+        address winnerAddress = players[index];
+        winnerAddress.transfer(this.balance);
+        players = new address[](1);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+}
---
+++ lottery/deploy.js
@@ -0,0 +1,26 @@
+// deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const infura_api_endpoint =
+  'https://rinkeby.infura.io/v3/685f851de0d84f6989dc57e8239e5dd6';
+
+const provider = new HDWalletProvider(
+  'clown enjoy case clump close potato entry anxiety wreck dial memory donor',
+  infura_api_endpoint
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const [account, ...rest] = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', account);
+  const contract = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: account });
+  console.log('contract deployed to:', contract.options.address);
+};
+
+deploy();
