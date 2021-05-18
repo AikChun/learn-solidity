8471babd3d1b40cd2f781efc46425b826d5ca515
AikChun <iam@simaikchun.com>
2021-05-18 16:14:01 +0800

    Added the interface and address of the contract into the comments section.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import web3 from './web3';
 import './App.css';
 
+/**
+ * 0x445A13f8B25fcdc7663Dada23b22D16E17275929
+ * [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+ */
+
 function App() {
   console.log(web3.version);
   web3.eth.getAccounts().then(console.log);
