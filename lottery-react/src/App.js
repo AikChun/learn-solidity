8570701726bd5e9b3e2ea6607413c8f595fa1e79
AikChun <iam@simaikchun.com>
2021-05-19 10:53:22 +0800

    Added a file to export a local instance of the lottery contract in the react application. Added implementation to App to fetch manager address on mount.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,31 +1,22 @@
-import logo from './logo.svg';
-import web3 from './web3';
+import { useEffect, useState } from 'react';
+import lottery from './lottery';
 import './App.css';
 
-/**
- * 0x445A13f8B25fcdc7663Dada23b22D16E17275929
- * [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
- */
-
 function App() {
-  console.log(web3.version);
-  web3.eth.getAccounts().then(console.log);
+  const [manager, setManager] = useState('');
+
+  const fetchManagerAddress = async () => {
+    const managerAddress = await lottery.methods.manager().call();
+    setManager(managerAddress);
+  };
+
+  useEffect(() => {
+    fetchManagerAddress();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager}</p>
     </div>
   );
 }
