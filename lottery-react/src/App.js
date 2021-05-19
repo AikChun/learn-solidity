bbed0d47b2a4af793eb255eebd61e5dbe92082f4
AikChun <iam@simaikchun.com>
2021-05-19 11:07:36 +0800

    Added implementation to fetch and show the number of players who have entered the lottery and also amount of ether as the prize.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,22 +1,33 @@
 import { useEffect, useState } from 'react';
 import lottery from './lottery';
 import './App.css';
+import web3 from './web3';
 
 function App() {
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
 
-  const fetchManagerAddress = async () => {
+  const fetchContractDataAddress = async () => {
     const managerAddress = await lottery.methods.manager().call();
     setManager(managerAddress);
+    const playersList = await lottery.methods.getPlayers().call();
+    setPlayers(playersList);
+    const contractBalance = await web3.eth.getBalance(lottery.options.address);
+    setBalance(contractBalance);
   };
 
   useEffect(() => {
-    fetchManagerAddress();
+    fetchContractDataAddress();
   }, []);
   return (
     <div>
       <h2>Lottery Contract</h2>
       <p>This contract is managed by {manager}</p>
+      <p>
+        There are currently {players.length} people entered, competing to win{' '}
+        {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
     </div>
   );
 }
