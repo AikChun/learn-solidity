a393597610fd7c71b7cc41116efa0924c5f5775d
AikChun <iam@simaikchun.com>
2021-05-19 15:51:12 +0800

    Added implementation to enter and pick out the winner for the lottery.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
   const [value, setValue] = useState('');
+  const [formMessage, setFormMessage] = useState('');
 
   const fetchContractDataAddress = async () => {
     const managerAddress = await lottery.methods.manager().call();
@@ -20,9 +21,28 @@ function App() {
 
   const onValueChange = (e) => setValue(e.target.value);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setFormMessage('Waiting on transaction success...');
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei(value, 'ether') });
+    setFormMessage('You have been entered!');
+  };
+
+  const onPick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setFormMessage('Waiting on transaction success...');
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+    setFormMessage('A winner has been picked!');
+  };
+
   useEffect(() => {
     fetchContractDataAddress();
-  }, []);
+  }, [players, balance]);
   return (
     <div>
       <h2>Lottery Contract</h2>
@@ -32,14 +52,21 @@ function App() {
         {web3.utils.fromWei(balance, 'ether')} ether!
       </p>
       <hr />
-      <form>
+      <form onSubmit={onSubmit}>
         <h4>Want to try your luck?</h4>
         <div>
           <label>Amount of ether to enter</label>
           <input onChange={onValueChange} value={value} />
         </div>
-        <button type="button">Enter</button>
+        <button type="submit">Enter</button>
       </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onPick} type="button">
+        Pick a winner
+      </button>
+      <hr />
+      <h1>{formMessage}</h1>
     </div>
   );
 }
