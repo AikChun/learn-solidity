ba4614388c5804260cc19a83bd97bb3c1168c18d
AikChun <iam@simaikchun.com>
2021-05-24 17:08:43 +0800

    Added layout component to be used to render all the common components for each page. Added a header component.

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Button } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
 import factory from '../ethereum/factory';
 
@@ -15,15 +16,17 @@ function Index({ campaigns = [] }) {
   };
 
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.3/dist/semantic.min.css"
-      />
-      <h3>Open Campaigns</h3>
-      {renderCampaigns()}
-      <Button content="Create Campaign" icon="add" primary />
-    </div>
+    <Layout>
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.3/dist/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+        {renderCampaigns()}
+        <Button content="Create Campaign" icon="add" primary />
+      </div>
+    </Layout>
   );
 }
 
