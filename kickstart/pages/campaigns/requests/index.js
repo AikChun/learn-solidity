f47d4e6ebaea4f9dd0813746b03365d91d60a085
AikChun <iam@simaikchun.com>
2021-05-27 16:39:11 +0800

    Added a new page and route for creating new requests.

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Layout from '../../../components/Layout';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
 
-const RequestIndex = () => {
-  return <h3>View Requests</h3>;
+const RequestIndex = (props) => {
+  const { address } = props;
+  return (
+    <Layout>
+      <h3>View Requests</h3>
+      <Link route={`/campaigns/${address}/requests/new`}>
+        <a>
+          <Button primary>Add Request</Button>
+        </a>
+      </Link>
+    </Layout>
+  );
+};
+
+RequestIndex.getInitialProps = (props) => {
+  const {
+    query: { address },
+  } = props;
+
+  return { address };
 };
 
 export default RequestIndex;
---
+++ kickstart/pages/campaigns/requests/new.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import Layout from '../../../components/Layout';
+import { Form, Input, Button, Message } from 'semantic-ui-react';
+import { Router } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+
+const RequestNew = (props) => {
+  const { address } = props;
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const campaign = Campaign(address);
+
+    setLoading(true);
+    setErrorMessage('');
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods
+        .createRequest(
+          description,
+          web3.utils.toWei(amount, 'ether'),
+          recipient
+        )
+        .send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${address}/requests`);
+    } catch (err) {
+      setErrorMessage(`Something went wrong. ${err.message}`);
+    }
+    setLoading(false);
+  };
+  return (
+    <Layout>
+      <h3>Create a Request</h3>
+      <Form error={!!errorMessage} onSubmit={onSubmit}>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Amount in Ether</label>
+          <Input
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={(e) => {
+              setRecipient(e.target.value);
+            }}
+          />
+        </Form.Field>
+        <Message error content={errorMessage} />
+        <Button type="submit" primary loading={loading}>
+          Create
+        </Button>
+      </Form>
+    </Layout>
+  );
+};
+
+RequestNew.getInitialProps = (props) => {
+  const {
+    query: { address },
+  } = props;
+  console.log('address', address);
+
+  return { address };
+};
+
+export default RequestNew;
