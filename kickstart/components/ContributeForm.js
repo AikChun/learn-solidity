322ebd63fd25aa0107a617925af06167855d55e5
AikChun <iam@simaikchun.com>
2021-05-27 10:46:12 +0800

    imported a router object to refresh the page after the form is successfully

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -3,20 +3,23 @@ import React, { useState } from 'react';
 import { Form, Button, Message, Input } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 const ContributeForm = (props) => {
   const [value, setValue] = useState('');
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log('props.address', props.address);
-    const campaign = Campaign(props.address);
+    const { address } = props;
+
+    const campaign = Campaign(address);
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.contribute().send({
         from: accounts[0],
         value: web3.utils.toWei(value, 'ether'),
       });
+      Router.replaceRoute(`/campaigns/${address}`);
     } catch (err) {}
   };
 
