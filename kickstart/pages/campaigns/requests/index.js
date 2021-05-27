c390ab8327b7297f8a33951b83d43f4c5c29f148
AikChun <iam@simaikchun.com>
2021-05-28 06:51:22 +0800

    Implemented a promise.all functionality to fetch all the requests that we have in the campaign.

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
+import web3 from '../../../ethereum/web3';
+import Campaign from '../../../ethereum/campaign';
 
 const RequestIndex = (props) => {
   const { address } = props;
+
+  const { Header, Row, HeaderCell, Body } = Table;
   return (
     <Layout>
       <h3>View Requests</h3>
@@ -13,16 +17,40 @@ const RequestIndex = (props) => {
           <Button primary>Add Request</Button>
         </a>
       </Link>
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approved</HeaderCell>
+            <HeaderCell>Finalized</HeaderCell>
+          </Row>
+        </Header>
+      </Table>
     </Layout>
   );
 };
 
-RequestIndex.getInitialProps = (props) => {
+RequestIndex.getInitialProps = async (props) => {
   const {
     query: { address },
   } = props;
 
-  return { address };
+  const campaign = Campaign(address);
+  const requestsCount = await campaign.methods.getRequestsCount().call();
+
+  const requests = await Promise.all(
+    Array(parseInt(requestsCount))
+      .fill()
+      .map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+  );
+
+  return { address, requests, requestsCount };
 };
 
 export default RequestIndex;
